ef3a71eb18168b3ac159144139f3181d624b9b54
base-13 <86418442+base-13@users.noreply.github.com>
2026-08-06 15:43:23 +0530

    move shifted registers instruction now updates condition flags

--- src/cpu/thumb/exec.zig
+++ src/cpu/thumb/exec.zig
@@ -6,11 +6,34 @@ pub fn execMoveRegister(instr: is.MoveRegisterTInstr, registers: *cpu_state.Regi
     const rs_content = registers.get(instr.rs);
 
     switch (instr.opcode) {
-        .LogicalLeft => value = rs_content << instr.offset,
-        .LogicalRight => value = rs_content >> instr.offset,
-        .ArithmeticRight => value = @bitCast(@as(i32, @bitCast(rs_content)) >> instr.offset),
+        .LogicalLeft => {
+            value = rs_content << instr.offset;
+
+            var carry: u1 = undefined;
+            if (instr.offset != 0)
+                carry = @truncate(rs_content >> (0 -% instr.offset)) // equivalent to 32 - instr.offset
+            else
+                carry = 0;
+
+            registers.cpsr.carry_flag = carry == 1;
+        },
+        .LogicalRight => {
+            value = rs_content >> instr.offset;
+            registers.cpsr.carry_flag = (rs_content >> (instr.offset - 1)) & 1 == 1;
+        },
+        .ArithmeticRight => {
+            const rs_signed: i32 = @bitCast(rs_content);
+
+            value = @bitCast(rs_signed >> instr.offset);
+
+            const carry = (rs_signed >> (instr.offset - 1)) & 1;
+            registers.cpsr.carry_flag = carry == 1;
+        },
         else => unreachable,
     }
+
+    registers.cpsr.neg_flag = (value >> 31) == 1;
+    registers.cpsr.zero_flag = value == 0;
 
     registers.set(instr.rd, value);
 
