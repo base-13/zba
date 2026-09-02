8c68d0e945b00e6230e0f96e654fb9420e0d1136
base-13 <86418442+base-13@users.noreply.github.com>
2026-09-02 14:27:11 +0530

    refactor IORegisters

--- src/io.zig
+++ src/io.zig
@@ -105,27 +105,11 @@ pub fn readIOR(ior: *IORegistersType, addr: u32, comptime length: Length) Length
 
 // these are meant to be used by other units such as PPU, Audio unit etc
 
-/// Writes `value` to given register, it is not meant for use by MMU
-///
-/// **Note:** This function ignores if register is writeable or not
-/// **Note:** This function doesn't check bounds on value it is writing
-pub fn setIOR(ior: *IORegistersType, reg: IORegisters, value: u32, comptime length: Length) LengthType(length) {
-    setIORByAddr(ior, @intFromEnum(reg), value, length);
-}
-
-/// Reads the given register, it is not meant for use by MMU
-///
-/// **Note:** This function ignores if register is readable or not
-/// **Note:** This function doesn't check bounds on value it is reading
-pub fn getIOR(ior: *IORegistersType, reg: IORegisters, comptime length: Length) LengthType(length) {
-    return getIORByAddr(ior, @intFromEnum(reg), length);
-}
-
 /// Writes `value` to given address(starting from 0x4000000), it is not meant for use by MMU
 ///
 /// **Note:** This function ignores if register is writeable or not
 /// **Note:** This function doesn't check bounds on value it is writing
-pub fn setIORByAddr(ior: *IORegistersType, addr: u32, value: u32, comptime length: Length) LengthType(length) {
+pub fn setIOR(ior: *IORegistersType, addr: u32, value: u32, comptime length: Length) LengthType(length) {
     const rel_addr = addr - 0x4000000;
 
     var buf: [@intFromEnum(length)]u8 = @splat(0);
@@ -143,7 +127,7 @@ pub fn setIORByAddr(ior: *IORegistersType, addr: u32, value: u32, comptime lengt
 ///
 /// **Note:** This function ignores if register is readable or not
 /// **Note:** This function doesn't check bounds on value it is reading
-pub fn getIORByAddr(ior: *IORegistersType, addr: u32, comptime length: Length) LengthType(length) {
+pub fn getIOR(ior: *IORegistersType, addr: u32, comptime length: Length) LengthType(length) {
     const rel_addr = addr - 0x4000000;
 
     const read_size = @intFromEnum(length);
@@ -159,119 +143,119 @@ pub fn getIORByAddr(ior: *IORegistersType, addr: u32, comptime length: Length) L
     return std.mem.readInt(LengthType(length), &buf, .little);
 }
 
-pub const IORegisters = enum(u32) {
+pub const IORegisters = struct {
     // LCD
-    DISPCNT = 0x4000000,
-    DISPSTAT = 0x4000004,
-    VCOUNT = 0x4000006,
-    BG0CNT = 0x4000008,
-    BG1CNT = 0x400000A,
-    BG2CNT = 0x400000C,
-    BG3CNT = 0x400000E,
-    BG0HOFS = 0x4000010,
-    BG0VOFS = 0x4000012,
-    BG1HOFS = 0x4000014,
-    BG1VOFS = 0x4000016,
-    BG2HOFS = 0x4000018,
-    BG2VOFS = 0x400001A,
-    BG3HOFS = 0x400001C,
-    BG3VOFS = 0x400001E,
-    BG2PA = 0x4000020,
-    BG2PB = 0x4000022,
-    BG2PC = 0x4000024,
-    BG2PD = 0x4000026,
-    BG2X = 0x4000028,
-    BG2Y = 0x400002C,
-    BG3PA = 0x4000030,
-    BG3PB = 0x4000032,
-    BG3PC = 0x4000034,
-    BG3PD = 0x4000036,
-    BG3X = 0x4000038,
-    BG3Y = 0x400003C,
-    WIN0H = 0x4000040,
-    WIN1H = 0x4000042,
-    WIN0V = 0x4000044,
-    WIN1V = 0x4000046,
-    WININ = 0x4000048,
-    WINOUT = 0x400004A,
-    MOSAIC = 0x400004C,
-    BLDCNT = 0x4000050,
-    BLDALPHA = 0x4000052,
-    BLDY = 0x4000054,
+    pub const DISPCNT: u32 = 0x4000000;
+    pub const DISPSTAT: u32 = 0x4000004;
+    pub const VCOUNT: u32 = 0x4000006;
+    pub const BG0CNT: u32 = 0x4000008;
+    pub const BG1CNT: u32 = 0x400000A;
+    pub const BG2CNT: u32 = 0x400000C;
+    pub const BG3CNT: u32 = 0x400000E;
+    pub const BG0HOFS: u32 = 0x4000010;
+    pub const BG0VOFS: u32 = 0x4000012;
+    pub const BG1HOFS: u32 = 0x4000014;
+    pub const BG1VOFS: u32 = 0x4000016;
+    pub const BG2HOFS: u32 = 0x4000018;
+    pub const BG2VOFS: u32 = 0x400001A;
+    pub const BG3HOFS: u32 = 0x400001C;
+    pub const BG3VOFS: u32 = 0x400001E;
+    pub const BG2PA: u32 = 0x4000020;
+    pub const BG2PB: u32 = 0x4000022;
+    pub const BG2PC: u32 = 0x4000024;
+    pub const BG2PD: u32 = 0x4000026;
+    pub const BG2X: u32 = 0x4000028;
+    pub const BG2Y: u32 = 0x400002C;
+    pub const BG3PA: u32 = 0x4000030;
+    pub const BG3PB: u32 = 0x4000032;
+    pub const BG3PC: u32 = 0x4000034;
+    pub const BG3PD: u32 = 0x4000036;
+    pub const BG3X: u32 = 0x4000038;
+    pub const BG3Y: u32 = 0x400003C;
+    pub const WIN0H: u32 = 0x4000040;
+    pub const WIN1H: u32 = 0x4000042;
+    pub const WIN0V: u32 = 0x4000044;
+    pub const WIN1V: u32 = 0x4000046;
+    pub const WININ: u32 = 0x4000048;
+    pub const WINOUT: u32 = 0x400004A;
+    pub const MOSAIC: u32 = 0x400004C;
+    pub const BLDCNT: u32 = 0x4000050;
+    pub const BLDALPHA: u32 = 0x4000052;
+    pub const BLDY: u32 = 0x4000054;
 
     // Sound
-    SOUND1CNT_L = 0x4000060,
-    SOUND1CNT_H = 0x4000062,
-    SOUND1CNT_X = 0x4000064,
-    SOUND2CNT_L = 0x4000068,
-    SOUND2CNT_H = 0x400006C,
-    SOUND3CNT_L = 0x4000070,
-    SOUND3CNT_H = 0x4000072,
-    SOUND3CNT_X = 0x4000074,
-    SOUND4CNT_L = 0x4000078,
-    SOUND4CNT_H = 0x400007C,
-    SOUNDCNT_L = 0x4000080,
-    SOUNDCNT_H = 0x4000082,
-    SOUNDCNT_X = 0x4000084,
-    SOUNDBIAS = 0x4000088,
-    WAVE_RAM = 0x4000090,
-    FIFO_A = 0x40000A0,
-    FIFO_B = 0x40000A4,
+    pub const SOUND1CNT_L: u32 = 0x4000060;
+    pub const SOUND1CNT_H: u32 = 0x4000062;
+    pub const SOUND1CNT_X: u32 = 0x4000064;
+    pub const SOUND2CNT_L: u32 = 0x4000068;
+    pub const SOUND2CNT_H: u32 = 0x400006C;
+    pub const SOUND3CNT_L: u32 = 0x4000070;
+    pub const SOUND3CNT_H: u32 = 0x4000072;
+    pub const SOUND3CNT_X: u32 = 0x4000074;
+    pub const SOUND4CNT_L: u32 = 0x4000078;
+    pub const SOUND4CNT_H: u32 = 0x400007C;
+    pub const SOUNDCNT_L: u32 = 0x4000080;
+    pub const SOUNDCNT_H: u32 = 0x4000082;
+    pub const SOUNDCNT_X: u32 = 0x4000084;
+    pub const SOUNDBIAS: u32 = 0x4000088;
+    pub const WAVE_RAM: u32 = 0x4000090;
+    pub const FIFO_A: u32 = 0x40000A0;
+    pub const FIFO_B: u32 = 0x40000A4;
 
     // DMA
-    DMA0SAD = 0x40000B0,
-    DMA0DAD = 0x40000B4,
-    DMA0CNT_L = 0x40000B8,
-    DMA0CNT_H = 0x40000BA,
-    DMA1SAD = 0x40000BC,
-    DMA1DAD = 0x40000C0,
-    DMA1CNT_L = 0x40000C4,
-    DMA1CNT_H = 0x40000C6,
-    DMA2SAD = 0x40000C8,
-    DMA2DAD = 0x40000CC,
-    DMA2CNT_L = 0x40000D0,
-    DMA2CNT_H = 0x40000D2,
-    DMA3SAD = 0x40000D4,
-    DMA3DAD = 0x40000D8,
-    DMA3CNT_L = 0x40000DC,
-    DMA3CNT_H = 0x40000DE,
+    pub const DMA0SAD: u32 = 0x40000B0;
+    pub const DMA0DAD: u32 = 0x40000B4;
+    pub const DMA0CNT_L: u32 = 0x40000B8;
+    pub const DMA0CNT_H: u32 = 0x40000BA;
+    pub const DMA1SAD: u32 = 0x40000BC;
+    pub const DMA1DAD: u32 = 0x40000C0;
+    pub const DMA1CNT_L: u32 = 0x40000C4;
+    pub const DMA1CNT_H: u32 = 0x40000C6;
+    pub const DMA2SAD: u32 = 0x40000C8;
+    pub const DMA2DAD: u32 = 0x40000CC;
+    pub const DMA2CNT_L: u32 = 0x40000D0;
+    pub const DMA2CNT_H: u32 = 0x40000D2;
+    pub const DMA3SAD: u32 = 0x40000D4;
+    pub const DMA3DAD: u32 = 0x40000D8;
+    pub const DMA3CNT_L: u32 = 0x40000DC;
+    pub const DMA3CNT_H: u32 = 0x40000DE;
 
     // Timer
-    TM0CNT_L = 0x4000100,
-    TM0CNT_H = 0x4000102,
-    TM1CNT_L = 0x4000104,
-    TM1CNT_H = 0x4000106,
-    TM2CNT_L = 0x4000108,
-    TM2CNT_H = 0x400010A,
-    TM3CNT_L = 0x400010C,
-    TM3CNT_H = 0x400010E,
+    pub const TM0CNT_L: u32 = 0x4000100;
+    pub const TM0CNT_H: u32 = 0x4000102;
+    pub const TM1CNT_L: u32 = 0x4000104;
+    pub const TM1CNT_H: u32 = 0x4000106;
+    pub const TM2CNT_L: u32 = 0x4000108;
+    pub const TM2CNT_H: u32 = 0x400010A;
+    pub const TM3CNT_L: u32 = 0x400010C;
+    pub const TM3CNT_H: u32 = 0x400010E;
 
     // Serial 1
-    SIODATA32 = 0x4000120,
-    SIOMULTI0 = 0x4000120,
-    SIOMULTI1 = 0x4000122,
-    SIOMULTI2 = 0x4000124,
-    SIOMULTI3 = 0x4000126,
-    SIOCNT = 0x4000128,
-    SIOMLT_SEND = 0x400012A,
-    SIODATA8 = 0x400012A,
+    pub const SIODATA32: u32 = 0x4000120;
+    pub const SIOMULTI0: u32 = 0x4000120;
+    pub const SIOMULTI1: u32 = 0x4000122;
+    pub const SIOMULTI2: u32 = 0x4000124;
+    pub const SIOMULTI3: u32 = 0x4000126;
+    pub const SIOCNT: u32 = 0x4000128;
+    pub const SIOMLT_SEND: u32 = 0x400012A;
+    pub const SIODATA8: u32 = 0x400012A;
 
     // Keypad
-    KEYINPUT = 0x4000130,
-    KEYCNT = 0x4000132,
+    pub const KEYINPUT: u32 = 0x4000130;
+    pub const KEYCNT: u32 = 0x4000132;
 
     // Serial 2
-    RCNT = 0x4000134,
-    JOYCNT = 0x4000140,
-    JOY_RECV = 0x4000150,
-    JOY_TRANS = 0x4000154,
-    JOYSTAT = 0x4000158,
+    pub const RCNT: u32 = 0x4000134;
+    pub const JOYCNT: u32 = 0x4000140;
+    pub const JOY_RECV: u32 = 0x4000150;
+    pub const JOY_TRANS: u32 = 0x4000154;
+    pub const JOYSTAT: u32 = 0x4000158;
 
     // Interrupt, Waitstate, and Power-Down Control
-    IE = 0x4000200,
-    IF = 0x4000202,
-    WAITCNT = 0x4000204,
-    IME = 0x4000208,
-    POSTFLG = 0x4000300,
-    HALTCNT = 0x4000301,
+    pub const IE: u32 = 0x4000200;
+    pub const IF: u32 = 0x4000202;
+    pub const WAITCNT: u32 = 0x4000204;
+    pub const IME: u32 = 0x4000208;
+    pub const POSTFLG: u32 = 0x4000300;
+    pub const HALTCNT: u32 = 0x4000301;
 };
